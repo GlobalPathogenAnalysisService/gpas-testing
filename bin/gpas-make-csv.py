0c1665256c83a78bf0ab5096c9b7bfc66fd4f345
Philip W Fowler <philipwfowler@users.noreply.github.com>
2022-04-04 11:48:37 +0100

    make csv can handle new upload CSV format

--- bin/gpas-make-csv.py
+++ bin/gpas-make-csv.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--organisation",default="University of Oxford",help="the name of the organisation (the user must belong to it otherwise validation will fail)")
+    parser.add_argument("--old_format", action="store_true", help="whether to use the original upload CSV format and headers")
     parser.add_argument("--country",default="United Kingdom",help="the name of the country where the samples were collected")
     parser.add_argument("--tech",default='illumina',help="whether to generate illumina (paired) or nanopore (unpaired) reads")
     parser.add_argument("--file_type",default='fastq',help="whether to look for FASTQ or BAM files")
@@ -28,21 +29,30 @@ if __name__ == "__main__":
 
     if options.file_type == 'fastq':
         if options.tech == 'illumina':
-            header='name,fastq1,fastq2,organisation,tags,specimenOrganism,host,collectionDate,country,submissionTitle,submissionDescription,instrument_platform,instrument_model,flowcell'
+            if options.old_format:
+                header = 'name,fastq1,fastq2,organisation,tags,specimenOrganism,host,collectionDate,country,submissionTitle,submissionDescription,instrument_platform,instrument_model,flowcell'
+            else:
+                header = 'batch,run_number,sample_name,bam,control,collection_date,tags,country,region,district,specimen_organism,host,instrument_platform,primer_scheme'
             file_list = glob.glob('*_1.fastq.gz')
             file_extensions = ['_1.fastq.gz','_2.fastq.gz']
         elif options.tech == 'nanopore':
-            header='name,fastq,organisation,tags,specimenOrganism,host,collectionDate,country,submissionTitle,submissionDescription,instrument_platform,instrument_model,flowcell'
+            if options.old_format:
+                header = 'name,fastq,organisation,tags,specimenOrganism,host,collectionDate,country,submissionTitle,submissionDescription,instrument_platform,instrument_model,flowcell'
+            else:
+                header = 'batch,run_number,sample_name,fastq,control,collection_date,tags,country,region,district,specimen_organism,host,instrument_platform,primer_scheme'
             file_list = glob.glob('*.fastq.gz')
             file_extensions = ['.fastq.gz']
     elif options.file_type == 'bam':
-        header='name,bam,organisation,tags,specimenOrganism,host,collectionDate,country,submissionTitle,submissionDescription,instrument_platform,instrument_model,flowcell'
+        if options.old_format:
+            header = 'name,bam,organisation,tags,specimenOrganism,host,collectionDate,country,submissionTitle,submissionDescription,instrument_platform,instrument_model,flowcell'
+        else:
+            header = 'batch,run_number,sample_name,bam,control,collection_date,tags,country,region,district,specimen_organism,host,instrument_platform,primer_scheme'
         file_list = glob.glob('*.bam')
         file_extensions = ['.bam']
 
     print(header)
 
-    def build_rest_of_line():
+    def build_rest_of_line_old():
         rest_of_line = options.organisation+','
         if options.number_of_tags==0:
             rest_of_line+=','
@@ -67,6 +77,38 @@ if __name__ == "__main__":
 
         return(rest_of_line)
 
+    def build_rest_of_line():
+        # no control at present
+        rest_of_line = ','
+        date_collected=str(date.today()-timedelta(days=random.choice(range(7))))
+        rest_of_line+=date_collected+','
+        if options.number_of_tags==0:
+            rest_of_line+=','
+        elif options.number_of_tags==len(tags):
+            for i in tags:
+                rest_of_line+=i+':'
+            rest_of_line=rest_of_line[:-1]+','
+        else:
+            for i in random.sample(tags,options.number_of_tags):
+                rest_of_line+=i+':'
+            rest_of_line=rest_of_line[:-1]+','
+        rest_of_line+=options.country+','
+        # adminstrative domain
+        rest_of_line += ','
+        # district
+        rest_of_line += ','
+        rest_of_line+='SARS-CoV-2,human,'
+        if options.tech=='illumina':
+            rest_of_line+="Illumina,auto"
+        elif options.tech=='nanopore':
+            rest_of_line+="Nanopore,auto"
+
+        return(rest_of_line)
+
+    if not options.old_format:
+        batch = 'B-' + str(uuid.uuid4())[-6:]
+        run = str(uuid.uuid4())[-5:]
+
     for i in file_list:
 
         if options.file_type == 'fastq':
@@ -87,17 +129,28 @@ if __name__ == "__main__":
         else:
             uid=str(uuid.uuid4())[-4:]
 
-        rest_of_line=build_rest_of_line()
+        if options.old_format:
+            rest_of_line=build_rest_of_line_old()
+        else:
+            rest_of_line=build_rest_of_line()
 
         for file_extension in file_extensions:
             os.rename(filename+file_extension,lineage+"_"+uid+file_extension)
 
         if options.file_type == 'fastq':
             if options.tech=='illumina':
-                line=lineage+"_"+uid+','+lineage+"_"+uid+'_1.fastq.gz,'+lineage+"_"+uid+'_2.fastq.gz,'+rest_of_line
+                if options.old_format:
+                    line=lineage+"_"+uid+','+lineage+"_"+uid+'_1.fastq.gz,'+lineage+"_"+uid+'_2.fastq.gz,'+rest_of_line
+                else:
+                    line=batch+','+run+','+lineage+'_'+uid+','+lineage+"_"+uid+'_1.fastq.gz,'+lineage+"_"+uid+'_2.fastq.gz,'+rest_of_line
             elif options.tech=='nanopore':
-                line=lineage+"_"+uid+','+lineage+"_"+uid+'.fastq.gz,'+rest_of_line
+                if options.old_format:
+                    line=lineage+"_"+uid+','+lineage+"_"+uid+'.fastq.gz,'+rest_of_line
+                else:
+                    line=batch+','+run+','+lineage+'_'+uid+','+lineage+"_"+uid+'.fastq.gz,'+rest_of_line
         else:
-            line=lineage+"_"+uid+','+lineage+"_"+uid+'.bam,'+rest_of_line
-
+            if options.old_format:
+                line=lineage+"_"+uid+','+lineage+"_"+uid+'.bam,'+rest_of_line
+            else:
+                line=batch+','+run+','+lineage+"_"+uid+','+lineage+"_"+uid+'.bam,'+rest_of_line
         print(line)
