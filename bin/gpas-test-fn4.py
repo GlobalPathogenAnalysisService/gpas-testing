e6b6084f54c98654ce98dbd072ef9596e50d8180
Ubuntu <chia.dorland@gmail.com>
2022-05-31 13:13:01 +0000

    after testing on DEV

--- bin/gpas-test-fn4.py
+++ bin/gpas-test-fn4.py
@@ -24,9 +24,10 @@ samples = list(mapping.gpas_sample_name)
 
 table=[]
 for idx,guid0 in enumerate(samples):
+    a = mapping.loc[mapping.gpas_sample_name == guid0]
     for guid1 in samples[idx+1:]:
-        a = mapping.loc[mapping.gpas_sample_name == guid0]
-        n_snps = int(a.N_SNPS)
+        b = mapping.loc[mapping.gpas_sample_name == guid1]
+        n_snps = abs(int(a.N_SNPS)+int(b.N_SNPS))
         if n_snps > 3:
             present = False
         else:
@@ -35,20 +36,37 @@ for idx,guid0 in enumerate(samples):
 api_calls = pandas.DataFrame(table, columns=['guid0','guid1','present','snp_distance'])        
 
 api_start = 'http://fn4-gpasdev-app.privsn001.zymkc.oraclevcn.com:5092/api/v2/'
-api_end = '/neighbours_within/3/in_format/1'
+api_end = '/neighbours_within/3/in_format/'
 
 def make_api_calls(row):
 
     passes=False
 
-    api_url = api_start + row.guid0 + api_end
+    api_url = api_start + row.guid0 + api_end + '3'
     a = requests.get(api_url)
-
     if a.ok:
-        for (guid1, distance) in a.json():
-            if row.guid1 == guid1 and row.present and row.snp_distance == distance:
-                passes = True
-    
+        if row.guid1 not in a.json():
+            if not row.present:
+                passes=True
+        else:
+            api_url = api_start + row.guid0 + api_end + '1'
+            a = requests.get(api_url)
+            if a.ok:
+                for (guid1, distance) in a.json():
+                    if row.present:
+                        if row.guid1 == guid1 and row.snp_distance == distance:
+                            passes = True
+                
+                
+    else:
+        print("failed")
+        
     return(passes)         
 
-api_calls['passes'] = api_calls.apply(make_api_calls, axis=1)  
+api_calls['passes'] = api_calls.apply(make_api_calls, axis=1)
+
+print(api_calls)
+
+api_calls.to_csv('test-fn4-results.csv')
+
+print((~api_calls.passes).sum()==0)
